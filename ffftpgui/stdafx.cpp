b559486483d9bb1100980d88bb8be9942f30e7fa
Karoshi-clown <73593645+hatomania@users.noreply.github.com>
2021-12-30 13:17:27 +0900

    ベースとなる翻訳ファイルの追加

--- ffftpgui/stdafx.cpp
+++ ffftpgui/stdafx.cpp
@@ -1,11 +1,20 @@
 ﻿#include "stdafx.h"
 
+#ifdef _MSC_VER
+// コンパイル時に文字列リテラルをUTF-8に変換する
+// Qtが扱う文字列はすべてUTF-8にしなければならないが
+// コンパイラはShift-JIS(CP932)に変換してしまうため
+// 全角文字列はQtのウィジェット上では文字化けしてしまう
+#pragma execution_character_set("utf-8")
+#endif
+
 #define IMPLEMENT_CONSTANT_CHAR(x, str) const char* const x = str;
-#define IMPLEMENT_CONSTANT_QSTRING(x, str) const QString x{ QObject::tr(QString::fromWCharArray(str).toStdString().c_str()) };
 
 IMPLEMENT_CONSTANT_CHAR(kEmptyString, "")
 IMPLEMENT_CONSTANT_CHAR(kResImage_closedfolder16x16, ":/resource/closedfolder16x16.png")
 IMPLEMENT_CONSTANT_CHAR(kResImage_desktop16x16, ":/resource/desktop16x16.png")
 
-IMPLEMENT_CONSTANT_QSTRING(kAskSaveCryptTitle, L"暗号化の状態の保存")
-IMPLEMENT_CONSTANT_QSTRING(kAskSaveCryptBody, L"現在の暗号化の状態を保存しますか？\n「はい」を選択すると次回から他の暗号化方式を試行しなくなります。")
+// ユーザーに表示する文字列には必ずQObject::tr()で囲ってください
+// この文字列は翻訳可能文字列となり、Qt Linguistソフトウェアでtsファイルを編集できるようになります
+const QString kAskSaveCryptTitle = QObject::tr("暗号化の状態の保存");
+const QString kAskSaveCryptBody = QObject::tr("現在の暗号化の状態を保存しますか？\n「はい」を選択すると次回から他の暗号化方式を試行しなくなります。");

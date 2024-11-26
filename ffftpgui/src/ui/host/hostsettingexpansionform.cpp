76e25a8171303b948468e03c8ce4b220f870fac6
hatomania <73593645+hatomania@users.noreply.github.com>
2024-11-26 13:43:02 +0900

    ffftpgui: made expansion form on host setting dialog.

---
+++ ffftpgui/src/ui/host/hostsettingexpansionform.cpp
@@ -0,0 +1,32 @@
+﻿#include "hostsettingexpansionform.hpp"
+#include "ui_hostsettingexpansionform.h"
+#include "stdafx.h"
+
+// D-Pointer(PImplメカニズム)による隠ぺいの実装
+class HostSettingExpansionForm::Private {
+public:
+    Private() {}
+    ~Private() {}
+    Ui::HostSettingExpansionForm ui;
+    HostSettingExpansionForm::Data data;
+};
+
+HostSettingExpansionForm::Data::Data() {}
+
+HostSettingExpansionForm::HostSettingExpansionForm(QWidget* parent)
+    : QWidget(parent), d_(new HostSettingExpansionForm::Private())
+{
+    d_->ui.setupUi(this);
+}
+
+void HostSettingExpansionForm::setData(const Data& data) const {
+}
+
+const HostSettingExpansionForm::Data& HostSettingExpansionForm::getData() const {
+//    static Data data;
+    return d_->data;
+}
+
+void HostSettingExpansionForm::setDataAsDefault() const {
+    this->setData(Data());
+}

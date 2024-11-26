7a2a289a722d336393ef0bf0aa70f5395b25ab2b
hatomania <73593645+hatomania@users.noreply.github.com>
2024-11-26 15:51:52 +0900

    ffftpgui: made special form on host setting dialog.

---
+++ ffftpgui/src/ui/host/hostsettingspecialform.cpp
@@ -0,0 +1,32 @@
+﻿#include "hostsettingspecialform.hpp"
+#include "ui_hostsettingspecialform.h"
+#include "stdafx.h"
+
+// D-Pointer(PImplメカニズム)による隠ぺいの実装
+class HostSettingSpecialForm::Private {
+public:
+    Private() {}
+    ~Private() {}
+    Ui::HostSettingSpecialForm ui;
+    HostSettingSpecialForm::Data data;
+};
+
+HostSettingSpecialForm::Data::Data() {}
+
+HostSettingSpecialForm::HostSettingSpecialForm(QWidget* parent)
+    : QWidget(parent), d_(new HostSettingSpecialForm::Private())
+{
+    d_->ui.setupUi(this);
+}
+
+void HostSettingSpecialForm::setData(const Data& data) const {
+}
+
+const HostSettingSpecialForm::Data& HostSettingSpecialForm::getData() const {
+//    static Data data;
+    return d_->data;
+}
+
+void HostSettingSpecialForm::setDataAsDefault() const {
+    this->setData(Data());
+}

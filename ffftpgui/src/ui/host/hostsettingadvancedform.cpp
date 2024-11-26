d5276065dcc2121b0ce8410686cda13706355bad
hatomania <73593645+hatomania@users.noreply.github.com>
2024-11-26 15:28:56 +0900

    ffftpgui: made advanced form on host setting dialog.

---
+++ ffftpgui/src/ui/host/hostsettingadvancedform.cpp
@@ -0,0 +1,32 @@
+﻿#include "hostsettingadvancedform.hpp"
+#include "ui_hostsettingadvancedform.h"
+#include "stdafx.h"
+
+// D-Pointer(PImplメカニズム)による隠ぺいの実装
+class HostSettingAdvancedForm::Private {
+public:
+    Private() {}
+    ~Private() {}
+    Ui::HostSettingAdvancedForm ui;
+    HostSettingAdvancedForm::Data data;
+};
+
+HostSettingAdvancedForm::Data::Data() {}
+
+HostSettingAdvancedForm::HostSettingAdvancedForm(QWidget* parent)
+    : QWidget(parent), d_(new HostSettingAdvancedForm::Private())
+{
+    d_->ui.setupUi(this);
+}
+
+void HostSettingAdvancedForm::setData(const Data& data) const {
+}
+
+const HostSettingAdvancedForm::Data& HostSettingAdvancedForm::getData() const {
+//    static Data data;
+    return d_->data;
+}
+
+void HostSettingAdvancedForm::setDataAsDefault() const {
+    this->setData(Data());
+}
---
+++ ffftpgui/src/ui/host/hostsettingadvancedform.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <QtWidgets/QWidget>
+
+class HostSettingAdvancedForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    struct Data {
+        Data();
+    };
+    explicit HostSettingAdvancedForm(QWidget *parent = Q_NULLPTR);
+    void setData(const Data& data) const;
+    const Data& getData() const;
+    void setDataAsDefault() const;
+
+private slots:
+
+private:
+    class Private;
+    Private* d_;
+    Q_DISABLE_COPY(HostSettingAdvancedForm)
+};

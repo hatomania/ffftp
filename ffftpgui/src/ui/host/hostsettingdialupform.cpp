948477feeac26b7bae92cc2139004e61a7e1f312
hatomania <73593645+hatomania@users.noreply.github.com>
2024-11-26 15:12:56 +0900

    ffftpgui: made dialup form on host setting dialog.

---
+++ ffftpgui/src/ui/host/hostsettingdialupform.cpp
@@ -0,0 +1,32 @@
+﻿#include "hostsettingdialupform.hpp"
+#include "ui_hostsettingdialupform.h"
+#include "stdafx.h"
+
+// D-Pointer(PImplメカニズム)による隠ぺいの実装
+class HostSettingDialupForm::Private {
+public:
+    Private() {}
+    ~Private() {}
+    Ui::HostSettingDialupForm ui;
+    HostSettingDialupForm::Data data;
+};
+
+HostSettingDialupForm::Data::Data() {}
+
+HostSettingDialupForm::HostSettingDialupForm(QWidget* parent)
+    : QWidget(parent), d_(new HostSettingDialupForm::Private())
+{
+    d_->ui.setupUi(this);
+}
+
+void HostSettingDialupForm::setData(const Data& data) const {
+}
+
+const HostSettingDialupForm::Data& HostSettingDialupForm::getData() const {
+//    static Data data;
+    return d_->data;
+}
+
+void HostSettingDialupForm::setDataAsDefault() const {
+    this->setData(Data());
+}

bcd0aab9c82b8c4c955b4ba1a3250605929aa725
hatomania <73593645+hatomania@users.noreply.github.com>
2024-11-26 15:37:20 +0900

    ffftpgui: made encrypt form on host setting dialog.

---
+++ ffftpgui/src/ui/host/hostsettingencryptform.cpp
@@ -0,0 +1,32 @@
+﻿#include "hostsettingencryptform.hpp"
+#include "ui_hostsettingencryptform.h"
+#include "stdafx.h"
+
+// D-Pointer(PImplメカニズム)による隠ぺいの実装
+class HostSettingEncryptForm::Private {
+public:
+    Private() {}
+    ~Private() {}
+    Ui::HostSettingEncryptForm ui;
+    HostSettingEncryptForm::Data data;
+};
+
+HostSettingEncryptForm::Data::Data() {}
+
+HostSettingEncryptForm::HostSettingEncryptForm(QWidget* parent)
+    : QWidget(parent), d_(new HostSettingEncryptForm::Private())
+{
+    d_->ui.setupUi(this);
+}
+
+void HostSettingEncryptForm::setData(const Data& data) const {
+}
+
+const HostSettingEncryptForm::Data& HostSettingEncryptForm::getData() const {
+//    static Data data;
+    return d_->data;
+}
+
+void HostSettingEncryptForm::setDataAsDefault() const {
+    this->setData(Data());
+}
---
+++ ffftpgui/src/ui/host/hostsettingencryptform.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <QtWidgets/QWidget>
+
+class HostSettingEncryptForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    struct Data {
+        Data();
+    };
+    explicit HostSettingEncryptForm(QWidget *parent = Q_NULLPTR);
+    void setData(const Data& data) const;
+    const Data& getData() const;
+    void setDataAsDefault() const;
+
+private slots:
+
+private:
+    class Private;
+    Private* d_;
+    Q_DISABLE_COPY(HostSettingEncryptForm)
+};

51756c881cefc99618db60e9a6be4d53557113f6
hatomania <73593645+hatomania@users.noreply.github.com>
2024-11-26 14:53:12 +0900

    ffftpgui: made char-code form on host setting dialog.

---
+++ ffftpgui/src/ui/host/hostsettingcharcodeform.cpp
@@ -0,0 +1,32 @@
+﻿#include "hostsettingcharcodeform.hpp"
+#include "ui_hostsettingcharcodeform.h"
+#include "stdafx.h"
+
+// D-Pointer(PImplメカニズム)による隠ぺいの実装
+class HostSettingCharCodeForm::Private {
+public:
+    Private() {}
+    ~Private() {}
+    Ui::HostSettingCharCodeForm ui;
+    HostSettingCharCodeForm::Data data;
+};
+
+HostSettingCharCodeForm::Data::Data() {}
+
+HostSettingCharCodeForm::HostSettingCharCodeForm(QWidget* parent)
+    : QWidget(parent), d_(new HostSettingCharCodeForm::Private())
+{
+    d_->ui.setupUi(this);
+}
+
+void HostSettingCharCodeForm::setData(const Data& data) const {
+}
+
+const HostSettingCharCodeForm::Data& HostSettingCharCodeForm::getData() const {
+//    static Data data;
+    return d_->data;
+}
+
+void HostSettingCharCodeForm::setDataAsDefault() const {
+    this->setData(Data());
+}
---
+++ ffftpgui/src/ui/host/hostsettingcharcodeform.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <QtWidgets/QWidget>
+
+class HostSettingCharCodeForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    struct Data {
+        Data();
+    };
+    explicit HostSettingCharCodeForm(QWidget *parent = Q_NULLPTR);
+    void setData(const Data& data) const;
+    const Data& getData() const;
+    void setDataAsDefault() const;
+
+private slots:
+
+private:
+    class Private;
+    Private* d_;
+    Q_DISABLE_COPY(HostSettingCharCodeForm)
+};

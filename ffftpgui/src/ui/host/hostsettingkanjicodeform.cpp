920fac4f86b5605193f9a8fe1bf04845e6df1c8b
hatomania <73593645+hatomania@users.noreply.github.com>
2024-12-07 23:10:27 +0900

    feature-newhost: renamed variables.

--- ffftpgui/src/ui/host/hostsettingkanjicodeform.cpp
+++ ffftpgui/src/ui/host/hostsettingkanjicodeform.cpp
@@ -61,27 +61,27 @@ void HostSettingKanjiCodeForm::updateUi(const BaseForm::Data& data) {
       {KanjiCode::kSJIS, {*d_->ui.radioButton_1_2_SJIS, true}},
       {KanjiCode::kJIS, {*d_->ui.radioButton_1_3_JIS, true}},
       {KanjiCode::kEUC, {*d_->ui.radioButton_1_4_EUC, true}},
-      {KanjiCode::kUTF8N, {*d_->ui.radioButton_1_5_UTF8, false}},
+      {KanjiCode::kUTF8N, {*d_->ui.radioButton_1_5_UTF8N, false}},
       {KanjiCode::kUTF8BOM, {*d_->ui.radioButton_1_6_UTF8BOM, false}},
   };
   const std::map<KanjiCode, const TValue> name_codes{
-      {KanjiCode::kAUTO, TValue{*d_->ui.radioButton_2_1_Auto, false}},
+      {KanjiCode::kAUTO, TValue{*d_->ui.radioButton_2_1_AUTO, false}},
       {KanjiCode::kSJIS, TValue{*d_->ui.radioButton_2_2_SJIS, false}},
       {KanjiCode::kJIS, TValue{*d_->ui.radioButton_2_3_JIS, true}},
       {KanjiCode::kEUC, TValue{*d_->ui.radioButton_2_4_EUC, true}},
       {KanjiCode::kSMH, TValue{*d_->ui.radioButton_2_5_SMB_HEX, false}},
       {KanjiCode::kSMC, TValue{*d_->ui.radioButton_2_6_SMB_CAP, false}},
       {KanjiCode::kUTF8N, TValue{*d_->ui.radioButton_2_7_UTF8, false}},
-      {KanjiCode::kUTF8HFSX, TValue{*d_->ui.radioButton_2_8_UTF8HFSp, false}},
+      {KanjiCode::kUTF8HFSX, TValue{*d_->ui.radioButton_2_8_UTF8HFSP, false}},
   };
   //host_codes[tdata.kanjicode].rbtn.setChecked(true);// ←この書き方だとなぜかエラーなる
   host_codes.at(tdata.kanjicode).rbtn.setChecked(true);
-  UI_SETCHECKED(d_->ui.checkBox_1_1_HKanaToW, tdata.kanacnv);
-  UI_SETENABLED(d_->ui.checkBox_1_1_HKanaToW,
+  UI_SETCHECKED(d_->ui.checkBox_1_1_KanaConv, tdata.kanacnv);
+  UI_SETENABLED(d_->ui.checkBox_1_1_KanaConv,
                 host_codes.find(tdata.kanjicode)->second.conv);
   name_codes.at(tdata.kanjicode_name).rbtn.setChecked(true);
-  UI_SETCHECKED(d_->ui.checkBox_2_1_HKanaToW, tdata.kanacnv_name);
-  UI_SETENABLED(d_->ui.checkBox_2_1_HKanaToW,
+  UI_SETCHECKED(d_->ui.checkBox_2_1_KanaConv, tdata.kanacnv_name);
+  UI_SETENABLED(d_->ui.checkBox_2_1_KanaConv,
                 name_codes.find(tdata.kanjicode_name)->second.conv);
 }
 
@@ -92,18 +92,18 @@ void HostSettingKanjiCodeForm::updateData(BaseForm::Data& data) const {
       {KanjiCode::kSJIS, *d_->ui.radioButton_1_2_SJIS},
       {KanjiCode::kJIS, *d_->ui.radioButton_1_3_JIS},
       {KanjiCode::kEUC, *d_->ui.radioButton_1_4_EUC},
-      {KanjiCode::kUTF8N, *d_->ui.radioButton_1_5_UTF8},
+      {KanjiCode::kUTF8N, *d_->ui.radioButton_1_5_UTF8N},
       {KanjiCode::kUTF8BOM, *d_->ui.radioButton_1_6_UTF8BOM},
   };
   const std::map<KanjiCode, const QRadioButton&> name_codes{
-      {KanjiCode::kAUTO, *d_->ui.radioButton_2_1_Auto},
+      {KanjiCode::kAUTO, *d_->ui.radioButton_2_1_AUTO},
       {KanjiCode::kSJIS, *d_->ui.radioButton_2_2_SJIS},
       {KanjiCode::kJIS, *d_->ui.radioButton_2_3_JIS},
       {KanjiCode::kEUC, *d_->ui.radioButton_2_4_EUC},
       {KanjiCode::kSMH, *d_->ui.radioButton_2_5_SMB_HEX},
       {KanjiCode::kSMC, *d_->ui.radioButton_2_6_SMB_CAP},
       {KanjiCode::kUTF8N, *d_->ui.radioButton_2_7_UTF8},
-      {KanjiCode::kUTF8HFSX, *d_->ui.radioButton_2_8_UTF8HFSp},
+      {KanjiCode::kUTF8HFSX, *d_->ui.radioButton_2_8_UTF8HFSP},
   };
   for (const auto& p : host_codes) {
     if (p.second.isChecked()) {
@@ -111,14 +111,14 @@ void HostSettingKanjiCodeForm::updateData(BaseForm::Data& data) const {
       break;
     }
   }
-  UI_ISCHECKED(tdata.kanacnv, d_->ui.checkBox_1_1_HKanaToW);
+  UI_ISCHECKED(tdata.kanacnv, d_->ui.checkBox_1_1_KanaConv);
   for (const auto& p : name_codes) {
     if (p.second.isChecked()) {
       tdata.kanjicode_name = p.first;
       break;
     }
   }
-  UI_ISCHECKED(tdata.kanacnv_name, d_->ui.checkBox_2_1_HKanaToW);
+  UI_ISCHECKED(tdata.kanacnv_name, d_->ui.checkBox_2_1_KanaConv);
 }
 
 void HostSettingKanjiCodeForm::onClick_radioButton_Host() {
